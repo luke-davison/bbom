2b58f6103bd80971974a4a81b3b1ece8cf47c317
luke.davison@enlighten.co.nz <luke.davison@enlighten.co.nz>
2018-08-23 13:39:53 +1200

    Added more choose functions

--- src/state/gameFunctions/choose.ts
+++ src/state/gameFunctions/choose.ts
@@ -1,6 +1,61 @@
 import { IPlayerCard } from "../interfaces/IPlayerCard";
 
-export function choose(cards: IPlayerCard[], num: number, callback: (cards: IPlayerCard[]) => any): void {
+export function choose(cards: IPlayerCard[], num: number, callback: (chosen: IPlayerCard[], other: IPlayerCard[]) => any): void {
     console.log("choosing");
     // callback(cards[0]);
+}
+
+export function chooseFrom(cards: IPlayerCard[], num: number, callback: (cards: IPlayerCard[]) => any): void {
+    const arr: IPlayerCard[] = [];
+    while (cards.length) {
+        const card = cards.pop();
+        if (card) {
+            arr.push(card);
+        }
+    }
+    if (arr.length <= num) {
+        return callback(arr);
+    }
+    choose(arr, num, (chosen: IPlayerCard[], other: IPlayerCard[]) => {
+        while (other) {
+            const card = other.pop();
+            if (card) {
+                cards.push(card);
+            }
+        }
+        return callback(chosen);
+    });
+}
+
+export function chooseFromFiltered(cards: IPlayerCard[], num: number, filter: (card: IPlayerCard) => boolean, callback: (cards: IPlayerCard[]) => any): void {
+    const arr: IPlayerCard[] = [];
+    const arr2: IPlayerCard[] = [];
+    while (cards.length) {
+        const card = cards.pop();
+        if (card) {
+            if (filter(card)) {
+                arr.push(card);
+            } else {
+                arr2.push(card);
+            }
+        }
+    }
+    while (arr2) {
+        const card = arr2.pop();
+        if (card) {
+            cards.push(card);
+        }
+    }
+    if (arr.length <= num) {
+        return callback(arr);
+    }
+    choose(arr, num, (chosen: IPlayerCard[], other: IPlayerCard[]) => {
+        while (other) {
+            const card = other.pop();
+            if (card) {
+                cards.push(card);
+            }
+        }
+        return callback(chosen);
+    });
 }
